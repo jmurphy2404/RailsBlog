e98981d272d06404a135b6e340fd035e92c8456e
Elise Barnes <barnese3@go.stockton.edu>
2017-10-25 16:53:39 -0400

    photo uploader is working, created home page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:user_id, :body, :title)
+      params.require(:post).permit(:user_id, :body, :title, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20171025152208) do
     t.integer "user_id"
     t.string "body"
     t.string "title"
-    t.string "picture"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
