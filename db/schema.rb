c00a9ac30732da784dc450bd260f24e87ead92e9
John Murphy <murf2073@gmail.com>
2017-10-25 16:54:06 -0400

    updated readme

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20171025152208) do
     t.string "title"
     t.string "picture"
     t.string "location"
+    t.integer "latitude"
+    t.integer "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
