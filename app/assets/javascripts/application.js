dd34c259d0488d1f5ba16bfa8dfb577d47b226cb
Elise Barnes <barnese3@go.stockton.edu>
2017-10-27 10:21:30 -0400

    progress on home page and linking pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,33 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("DOMContentLoaded", function() {
+	document.querySelector(".signupbutton").addEventListener("click", function () {
+		document.querySelector(".signupmodal").style.display = "block";
+	})
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+	document.querySelector(".sign").addEventListener("click", function () {
+		document.querySelector(".signupmodal").style.display = "block";
+	})
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+document.querySelector(".signupclose").addEventListener("click", function (){
+	document.querySelector(".signupmodal").style.display = "none";
+	})
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+	document.querySelector(".login").addEventListener("click", function () {
+		document.querySelector(".loginmodal").style.display = "block";
+	})
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+document.querySelector(".loginclose").addEventListener("click", function (){
+	document.querySelector(".loginmodal").style.display = "none";
+	})
+})
