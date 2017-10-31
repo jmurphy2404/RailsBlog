efe89a8fb4f6e3318e8148c89fd3e82237e87cdb
John Murphy <murf2073@gmail.com>
2017-10-31 10:34:41 -0400

    updated styling and minor fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user, :optional => true
-  has_many :comments
+  has_many :comments, :dependent => :delete_all
   mount_uploader :image, ImageUploader
 end
+
+
+
+## user comments, 
+## delete comments by others on user posts, 
+## delete user posts, 
+## delete user itself 
