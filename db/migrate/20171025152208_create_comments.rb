face0a827d67f769d150b146d27b5963ea895176
Elise Barnes <barnese3@go.stockton.edu>
2017-11-06 17:23:54 -0500

    carrierwave added and working

--- db/migrate/20171025152208_create_comments.rb
+++ db/migrate/20171025152208_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.references :post, foreign_key: {to_table: :posts}, index: true
-  		t.references :user, foreign_key: {to_table: :users}, index: true
+  	  t.references :user, foreign_key: {to_table: :users}, index: true
       t.string :comment
 
       t.timestamps
