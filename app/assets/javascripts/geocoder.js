6164b9eef507ba34e987e1caf493941cfbba39f0
John Murphy <murf2073@gmail.com>
2017-11-15 11:07:28 -0500

    updated nav bar to include new post link on all pages and corrected bug in geocoder as well as links on post pages

--- app/assets/javascripts/geocoder.js
+++ app/assets/javascripts/geocoder.js
@@ -2,9 +2,6 @@ let geocoder;
 let address;
 let coords;
 
-
-
-
 document.addEventListener("DOMContentLoaded", function(){
 	geocoder = new google.maps.Geocoder();
 	address = document.getElementById("location")
@@ -15,16 +12,13 @@ document.addEventListener("DOMContentLoaded", function(){
 		var temp2 = results[0].geometry.location.lng();
 		document.getElementById("latitude").value = temp1;
 		document.getElementById("longitude").value = temp2;
-	})
-	
+		})
 	}
-
 	document.getElementById("geocoder_submit").addEventListener("click", function(e){
 		e.preventDefault();
 		getPlace(address.value);
 		document.getElementById("submit-post").style.visibility = "visible";
 		document.getElementById("geocoder_submit").style.visibility = "hidden";
-
 	})
 })
 
